deacdb5daf77e1fc43a663a1dea041b11059f294
Adofo90 <neymarjunior7853@gmail.com>
2026-09-07 20:53:18 +0530

    Create MainActivity with proper navigation listener implementation

---
+++ app/src/main/java/com/ghostie/pet/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ghostie.pet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    // Handle home navigation
+                    true
+                }
+                R.id.nav_settings -> {
+                    // Handle settings navigation
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
